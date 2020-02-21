d4599dbfb140cb35c209070aba45676b6d952b55
Colin Sheaff <colin-sheaff@northwestern.edu>
2020-02-21 15:18:41 -0600

    Modify LoginController.php to use the Drupal 7 password verification.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\Services\LegacyPassword;
+use App\User;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -19,7 +25,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers { attemptLogin as private attemptLaravelLogin; }
 
     /**
      * Where to redirect users after login.
@@ -37,4 +43,27 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        if ($this->attemptLaravelLogin($request)) {
+            return true;
+        }
+        $user = User::where('email', $request->request->get('email'))->first();
+        $password = $request->request->get('password');
+        if (!is_null($user) && LegacyPassword::match($password, $user->password)) {
+            Auth::login($user);
+            $user->update(['password' => Hash::make($password)]);
+            return true;
+        }
+        return false;
+    }
+
 }
+
